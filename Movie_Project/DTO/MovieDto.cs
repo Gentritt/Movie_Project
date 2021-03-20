cd8f53c42e4d649670d427ebd68e5c10befc1bb2
gentritt <gentrit.selimi@riinvest.net>
2021-03-20 19:36:47 +0100

    movieAPI

---
+++ Movie_Project/DTO/MovieDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movie_Project.DTO
+{
+	public class MovieDto
+	{
+
+		public int Id { get; set; }
+		[Required]
+		[StringLength(255)]
+		public string Name { get; set; }
+		[Required]
+		public byte GenreID { get; set; }
+		public DateTime DateAdded { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		[Range(1, 20)]
+		public byte NumerInStock { get; set; }
+	}
+}
